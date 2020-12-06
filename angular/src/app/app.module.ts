75ccb993581be5fa2fa88742583996217238a951
unknown <hurryy.info@gmail.com>
2020-12-06 22:04:06 +0100

    first commit

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
+import {MatCardModule} from '@angular/material/card';
+
+
+const modules = [
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  MatSliderModule,
+  MatCardModule,
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    modules,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    modules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
